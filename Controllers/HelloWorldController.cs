9e840f0fc14b51bbd1868949ceeacb90f92a96d9
Szabados Dodi <szabados.jozsefd@gmail.com>
2022-10-03 11:17:07 +0200

    First Commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers {
+    public class HelloWorldController : Controller {
+
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+        public IActionResult Index() {
+            //DogViewModel doggo = new DogViewModel() { Name = "Siff", Age = 2 };
+            //return View(doggo);
+
+            return View(dogs);
+        }
+        public IActionResult Create() {
+            var dogVm = new DogViewModel();
+
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel) {
+
+
+            //return View("Index");
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public string Hello() {
+            return "Who's there?";
+        }
+    }
+}
---
+++ Models/DogViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Models {
+    public class DogViewModel {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
